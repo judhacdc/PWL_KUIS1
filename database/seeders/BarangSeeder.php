a3df45ef4f234ce998b2a0a60771d948049da737
judhacdc <jdxxaa@gmail.com>
2022-03-15 22:47:32 +0700

    mengisi data barang

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BarangSeeder extends Seeder
 {
@@ -13,6 +14,49 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('barangs')->insert([
+            [
+                'nama' => 'Buku Tulis',
+                'harga' => 10000,
+                'gambar' => '/img/buku.png',
+                'kategori' => 'Alat Tulis',
+                'warna' => 'hijau',
+            ],
+            [
+                'nama' => 'Papan',
+                'harga' => 20000,
+                'gambar' => '/img/papan.png',
+                'kategori' => 'Alat Tulis',
+                'warna' => 'biru',
+            ],
+            [
+                'nama' => 'Pensil',
+                'harga' => 30000,
+                'gambar' => '/img/pensil.png',
+                'kategori' => 'Alat Tulis',
+                'warna' => 'Merah',
+            ],
+            [
+                'nama' => 'Sepatu',
+                'harga' => 40000,
+                'gambar' => '/img/sepatu.png',
+                'kategori' => 'Outfit Sekolah',
+                'warna' => 'Hitam',
+            ],
+            [
+                'nama' => 'Topi',
+                'harga' => 50000,
+                'gambar' => '/img/topi.png',
+                'kategori' => 'Outfit Sekolah',
+                'warna' => 'Maroon',
+            ],
+            [
+                'nama' => 'Dasi',
+                'harga' => 60000,
+                'gambar' => '/img/dasi.png',
+                'kategori' => 'Outfit Sekolah',
+                'warna' => 'Biru',
+            ]
+        ]);
     }
 }
