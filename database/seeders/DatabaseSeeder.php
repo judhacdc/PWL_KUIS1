f84ca1db5e65efee230709e0ecd3728f7d31e527
MahisaAghisni <aghisnifadhli@gmail.com>
2022-03-17 19:00:39 +0700

    mengisi database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([BarangSeeder::class, PelangganSeeder::class, PegawaiSeeder::class]);
+        $this->call([BarangSeeder::class, PelangganSeeder::class, PegawaiSeeder::class, SupplierSeeder::class]);
     }
 }
