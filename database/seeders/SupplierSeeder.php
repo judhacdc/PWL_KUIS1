097726e08faff51a34cef3fc38281b0f9daba20c
MahisaAghisni <aghisnifadhli@gmail.com>
2022-03-17 18:59:41 +0700

    mengisi Supplier Seeder

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SupplierSeeder extends Seeder
 {
@@ -13,6 +14,38 @@ class SupplierSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'namaPerusahaan' => 'PT Solo Murni',
+                'noTelpPerusahaan' => '081212145672',
+                'namaBarang' => 'Buku tulis',
+            ],
+            [
+                'namaPerusahaan' => 'PT Neon Box',
+                'noTelpPerusahaan' => '081212145673',
+                'namaBarang' => 'Papan',
+            ],
+            [
+                'namaPerusahaan' => 'PT Faber - Castel',
+                'noTelpPerusahaan' => '081212145674',
+                'namaBarang' => 'Pensil',
+            ],
+            [
+                'namaPerusahaan' => 'PT Nagasakti',
+                'noTelpPerusahaan' => '081212145675',
+                'namaBarang' => 'Sepatu',
+            ],
+            [
+                'namaPerusahaan' => 'PT Speed Jaya',
+                'noTelpPerusahaan' => '081212145676',
+                'namaBarang' => 'Topi',
+            ],
+            [
+                'namaPerusahaan' => 'PT Suban Cipta',
+                'noTelpPerusahaan' => '081212145677',
+                'namaBarang' => 'Dasi',
+            ]
+        ];
+        DB::table('Suppliers')->insert($data);
     }
 }
